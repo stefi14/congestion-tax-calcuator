3b53ab73b2a2490afd1886d306c609a5d15e6ccb
stefi14 <stefania.ramirez86@gmail.com>
2024-02-09 11:05:36 +0100

    small chnages

--- src/main/java/volvo/test/CongestionTaxCalculatorApplication.java
+++ src/main/java/volvo/test/CongestionTaxCalculatorApplication.java
@@ -2,12 +2,6 @@ package volvo.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import volvo.test.model.Car;
-import volvo.test.service.CongestionTaxCalculator;
-
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
 public class CongestionTaxCalculatorApplication {
--- src/main/java/volvo/test/repository/TollRepository.java
+++ src/main/java/volvo/test/repository/TollRepository.java
@@ -4,6 +4,4 @@ import org.springframework.data.repository.CrudRepository;
 import volvo.test.model.TollPrices;
 
 public interface TollRepository extends CrudRepository<TollPrices, Long> {
-
-
 }
